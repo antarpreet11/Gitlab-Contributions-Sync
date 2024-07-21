14a83c520c96a042962921b9b3d516a7bc0487e5
Antarpreet Singh <84466072+antarpreet11@users.noreply.github.com>
2024-07-20 23:38:26 -0400

    Added prov backend url to frontend

--- client/app/hooks/useSocket.ts
+++ client/app/hooks/useSocket.ts
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
 
-const WS_URL = 'ws://localhost:8080';
+const WS_URL = process.env.NEXT_PUBLIC_BACKEND_URL_PROD;
+// const WS_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export const useSocket = () => {
   const [socket, setSocket] = useState<WebSocket | null>(null);
 
     useEffect(() => {
-        const ws = new WebSocket(WS_URL);
+        const ws = new WebSocket(WS_URL as string);
 
         ws.onopen = () => {
             setSocket(ws);      
