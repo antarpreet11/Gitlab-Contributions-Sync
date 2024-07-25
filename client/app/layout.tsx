6a61facdd4d52e5b3e4bef3b9e6b5f5f40a2cd45
Antarpreet Singh <84466072+antarpreet11@users.noreply.github.com>
2024-07-25 18:45:44 -0400

    Fixed semicolon

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Analytics mode={'production'} />;
+        <Analytics mode={'production'} />
       </body>
     </html>
   );
